4e333aa82c538478f84e6b579436dfb6b3997489
kdepaula <42978472+kdepaula@users.noreply.github.com>
2018-09-17 22:10:57 -0400

    Add files via upload

---
+++ CalculatorM.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ * @author kdepa_t74iu3m
+ *
+ */
+public class CalculatorM 
+{
+
+	public static void main(String[] args) 
+	{
+		CalculatorS calc = new CalculatorS();
+		System.out.print(" The difference between " + 7 + " and " + 4 + " is " + calc.subtract(7, 4));
+
+	}
+
+}
---
+++ CalculatorS.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ * @author kdepa_t74iu3m
+ * Katelyn DePaula Period 7
+ */
+public class CalculatorS 
+{
+		
+	public int add(int num1, int num2)
+	{
+		int ans = num1 + num2;
+		return ans;
+	}
+	
+	public int subtract(int num1, int num2)
+	{
+			int ans = num1 - num2;
+			return ans;
+	}
+}
+
