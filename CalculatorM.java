f798ab31b8f079c8b3aa90c1ff7b2a66fdf7da68
kdepaula <42978472+kdepaula@users.noreply.github.com>
2018-09-18 14:28:03 -0400

    Add files via upload

--- CalculatorM.java
+++ CalculatorM.java
@@ -5,11 +5,15 @@
  */
 public class CalculatorM 
 {
-
+	//the line below this is the main method header
 	public static void main(String[] args) 
 	{
+		//CalculatorS calc;
+		///calc = new Calculator(); would also be the same thing as below
+		//you can declare and initialize in the same line
+		
 		CalculatorS calc = new CalculatorS();
-		System.out.print(" The difference between " + 7 + " and " + 4 + " is " + calc.subtract(7, 4));
+		System.out.print(" The difference is " + calc.subtract(7, 4));
 
 	}
 
--- CalculatorS.java
+++ CalculatorS.java
@@ -5,7 +5,7 @@
  */
 public class CalculatorS 
 {
-		
+		//the line below this the method header (not the main method just a method)
 	public int add(int num1, int num2)
 	{
 		int ans = num1 + num2;
@@ -14,8 +14,8 @@ public class CalculatorS
 	
 	public int subtract(int num1, int num2)
 	{
-			int ans = num1 - num2;
-			return ans;
+		
+			return num1 + num2;
 	}
 }
 
